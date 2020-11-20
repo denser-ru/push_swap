b1722c70f63cf27d8ce2df496508ce4ad986f571
Cheyenne Ayako <cayako@at-n3.msk.21-school.ru>
2020-11-20 15:44:17 +0300

    v2.0.1

--- src/testsort2.c
+++ src/testsort2.c
@@ -7,8 +7,6 @@
 
 int				ft_ps_get_m(t_ps *ps, t_swap *sw, size_t i, int *sort)
 {
-	while (sw && sw->chunk == 0)
-		sw = sw->prev;
 	while (sw)
 	{
 		sort[i] = *(sw->nb);
@@ -17,50 +15,56 @@ int				ft_ps_get_m(t_ps *ps, t_swap *sw, size_t i, int *sort)
 		if (sw && sw->next->chunk != sw->chunk)
 			break ;
 	}
-	ps->i =  i == 1 ? 1 : i / 2 ;
 	ft_sort_nb_arr(ps->sort, i);
-	return (sort[i / 2]);
+	i = i / 2;
+	ps->i = i;
+//	if (sort[i - 1] == 11)
+//		return (11);
+	return (sort[i - 1]);
 }
 
 void ft_ps_merge_sort(t_ps *ps, int i, int m, int ab)
 {
-	if (!ps->b->start && ft_lst_issorted(ps))
-		exit(0);
-	++ps->chunk;
-	if (!ps->st->start || ps->st->count <= 2 * ab)
-		ps->st = ps->st == ps->a ? ps->b : ps->a;
-	//	if (!(ps->st->count * ab))
-	m = ft_ps_get_m(ps, ps->st->end, 0, ps->sort);
-	if (i < 1)
+	int		chunk;
+	if (i < 2)
 		return ;
-	if (i < 2 && ((ab && ps->st->end->prev &&  *(ps->st->end->nb) > *(ps->st->end->prev->nb)) ||
-		(!ab && ps->st->end->prev && *(ps->st->end->nb) < *(ps->st->end->prev->nb))))
+	if (i == 2)
 	{
-		ft_add_cmd(ps, ab ? "sa\n" : "sb\n");
-		ft_put_cmd(ps, 1, 255);
-		usleep(ps->s);
+		if (ps->st->end->prev &&
+			((ab && *(ps->st->end->nb) > *(ps->st->end->prev->nb)) ||
+			 (!ab && *(ps->st->end->nb) < *(ps->st->end->prev->nb))))
+		{
+			ft_add_cmd(ps, ab ? "sa\n" : "sb\n");
+			ft_put_cmd(ps, 1, 255);
+			usleep(ps->s);
+		}
+		return ;
 	}
+	m = ft_ps_get_m(ps, ps->st->end, 0, ps->sort);
+	++ps->chunk;
+	chunk = ps->chunk;
+	ps->i = i / 2;
 	GOTOXY(54, 30);
-	ft_printf("\e[38;5;251mмедиана>: %-3d; i: %-2d count: %3d", m, ps->i, ps->st->count);
+	ft_printf("\e[38;5;251mмедиана: %-3d; i: %-2d count: %3d ab: %-2d", m, ps->i, ps->st->count, ab);
 	usleep(ps->s);
 	while (ps->i && ps->st->end && (ab ? *(ps->st->end->nb) <= m : *(ps->st->end->nb) >= m))
 	{
-//		if (ps->st->end)
-//			ps->st->end->chunk = ab ? ps->chunk : 1;
+		if (ps->st->end)
+			ps->st->end->chunk = ps->chunk;
 		ft_add_cmd(ps, ab ? "pb\n" : "pa\n");
 		ft_put_cmd(ps, 1, 255);
 		usleep(ps->s);
 	}
 	while (ps->i && ps->st->start && ps->st->end->chunk == ps->st->start->chunk
-		&& ps->st->start->chunk < 3 && (ab ? *(ps->st->start->nb) <= m :
+		&& ps->st->count > 2 && (ab ? *(ps->st->start->nb) <= m :
 		*(ps->st->start->nb) >= m))
 	{
 		ft_add_cmd(ps, ab ? "rra\n" : "rrb\n");
 		ft_put_cmd(ps, 1, 255);
 		usleep(ps->s);
-//		if (ps->st->end)
-//			ps->st->end->chunk = ab ? ps->chunk : 1;
-		ft_add_cmd(ps, "pb\n");
+		if (ps->st->end)
+			ps->st->end->chunk = ps->chunk;
+		ft_add_cmd(ps, ab ? "pb\n" : "pa\n");
 		ft_put_cmd(ps, 1, 255);
 		usleep(ps->s);
 	}
@@ -70,28 +74,38 @@ void ft_ps_merge_sort(t_ps *ps, int i, int m, int ab)
 		if (ab ? *(ps->st->end->nb) <= m : *(ps->st->end->nb) >= m)
 		{
 			if (ps->st->end)
-				ps->st->end->chunk = ab ? ps->chunk : 1;
+				ps->st->end->chunk = ps->chunk;
 			ft_add_cmd(ps, ab ? "pb\n" : "pa\n");
 		}
-		else
+		else if (ps->st->count > 2)
 			ft_add_cmd(ps, ab ? "ra\n" : "rb\n");
 		ft_put_cmd(ps, 1, 255);
 		usleep(ps->s);
 	}
-	ps->i =  i == 1 ? 1 : i / 2 ;
-	ft_ps_merge_sort(ps, ps->i, 0, ps->st == ps->a);
-
 	GOTOXY(54, 30);
-	ft_printf("\e[38;5;251mмедиана>: %-3d; i: %-2d", m, ps->i);
+	ft_printf("\e[38;5;251mмедиана: %-3d; i: %-2d count: %3d ab: %-2d", m, ps->i, ps->st->count, ab);
 	usleep(ps->s);
+//	ps->i = i / 2;
+		ps->st = ps->st == ps->a ? ps->b : ps->a;
+	ft_ps_merge_sort(ps, ps->i, 0, ps->st == ps->a);
+	GOTOXY(54, 30);
+	ft_printf("\e[38;5;251mмедиана: %-3d; i: %-2d count: %3d ab: %-2d", m, ps->i, ps->st->count, ab);
+	usleep(ps->s);
+	ps->i = i / 2;
+	ps->st = ps->st == ps->a ? ps->b : ps->a;
+	ft_ps_merge_sort(ps, ps->i, 0, ps->st == ps->a);
 }
 
 int				ft_push_swap(t_ps *ps, int mediana, int s)
 {
 	ps->s = s * 1000;
-	ps->i = ps->a->count / 2;
+	ps->i = ps->a->count;
 	ps->chunk = 1;
-	ft_ps_merge_sort(ps, ps->i, mediana, 1);
+	ft_ps_merge_sort(ps, ps->i , mediana, 1);
+	ps->chunk = 1;
+	ps->i = ps->b->count;
+	ps->st = ps->b;
+	ft_ps_merge_sort(ps, ps->i, mediana, 0);
 	GOTOXY(54, 35);
 	return (0);
 }
