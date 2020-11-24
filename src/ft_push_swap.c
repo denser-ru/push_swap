49cda2daa5014ff6c31b5978aba98cbea9f02079
denser-ru <mail@denser.ru>
2020-11-25 02:54:04 +0300

    v3.1.7

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cayako <cayako@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 01:13:28 by cayako            #+#    #+#             */
-/*   Updated: 2020/11/25 01:45:08 by cayako           ###   ########.fr       */
+/*   Updated: 2020/11/25 02:49:47 by cayako           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,12 @@ int			ft_check_duble(t_ps *ps, t_stack *st, t_swap *sw, int i)
 	{
 		sw = sw->prev;
 		++i;
-		if (i > 2 && sw && sw->next->chunk != sw->chunk)
-			return (0);
+		if (i > 2 || (sw->next->chunk != sw->chunk))
+			break ;
 	}
-	if (((ps->st == ps->a) && (*(st->end->nb) > *(st->end->prev->nb))) ||
+	if (i > 2)
+		return (0);
+	if ((ps->st == ps->a && (*(st->end->nb) > *(st->end->prev->nb))) ||
 			((ps->st == ps->b) && (*(st->end->nb) < *(st->end->prev->nb))))
 	{
 		ft_add_cmd(ps, ps->st == ps->a ? "sa\n" : "sb\n");
@@ -109,16 +111,15 @@ void	ft_ps_swap_ab(t_ps *ps, int m)
 {
 	while (1)
 	{
-		ft_check_duble(ps, ps->st, ps->st->end, 0);
 		m = ft_ps_get_med(ps, 2);
-		if (ft_check_duble(ps, ps->st, ps->st->end, 0))
-			m = ft_ps_get_med(ps, 2);
+		ft_check_duble(ps, ps->st, ps->st->end, 0);
 		ps->i = (ps->i + 1) / 2;
 		ft_ps_step_f(ps, m, ps->st == ps->a);
 		if ((ps->st == ps->a && ps->a->count < 3) || (!(ps->st == ps->a) && !ps->b->count))
 			ps->st = ps->st == ps->a ? ps->b : ps->a;
 	}
 }
+
 int				ft_push_swap(t_ps *ps, int m, int s)
 {
 	ps->s *= s;
