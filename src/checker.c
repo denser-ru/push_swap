56fc42d2ea555d0551d8cb3a5303e912326322d5
Cheyenne Ayako <cayako@oa-h3.msk.21-school.ru>
2021-02-10 16:17:08 +0300

    v3.5.5 finished without norme

--- src/checker.c
+++ src/checker.c
@@ -22,15 +22,47 @@ static int		ft_ps_read_in_txt(t_ps *ps, char *buf)
 	}
 }
 
+int			ft_add_cmd_in(t_ps *ps, char *buf)
+{
+    const char	*cmd[11] = {"sa\n", "sb\n", "ss\n", "pa\n", "pb\n", "ra\n",
+                              "rb\n", "rr\n", "rra\n", "rrb\n", "rrr\n"};
+    int			i;
+
+    i = 0;
+    while (cmd[i] && *buf && i < 11)
+    {
+        if (!ft_strcmp(cmd[i], buf))
+            return (ft_lstpushb(&(ps->cmds), buf, ft_strlen(buf) - 1) != NULL);
+        ++i;
+    }
+    GOTOXY(0, 47);
+    ft_printf(ESC "[0K");
+    ft_putendl("Error");
+    exit(1);
+}
+
 static int		ft_ps_read_stdin(t_ps *ps, char *buf)
 {
     int     i;
+    t_list  *cmds;
 
     i = 0;
     while (get_next_line(0, &buf))
     {
-        ft_put_cmd(ps, ps->cmds, 1, 255);
+        ft_putendl(buf);
+        buf[ft_strlen(buf)] = '\n';
+        ft_add_cmd_in(ps, buf);
+        ft_bzero(buf, 5);
         ++i;
+    }
+    cmds = ps->cmds;
+//    ft_putendl("-----");
+    while (cmds)
+    {
+        ft_do_cmd(ps, (char*)cmds->content);
+//        ft_putnstr((char*)cmds->content, cmds->content_size);
+//        ft_putchar('\n');
+        cmds = cmds->next;
     }
     return (i > 0);
 }
@@ -42,6 +74,20 @@ static void     ft_check_sorted(t_ps *ps)
     else
         ft_printf("KO\n");
 }
+
+//void	ft_print_sw_tmp(t_swap *sw)
+//{
+//    int		n;
+//
+//    n = 10;
+//    if (!sw)
+//        return ;
+//    while (sw && sw->nb && n > 1)
+//    {
+//        ft_printf("\e[38;5;%dm%-16d", 262 , *(sw->nb));
+//        sw = sw->next;
+//    }
+//}
 
 int				main(int argc, char **argv)
 {
@@ -69,6 +115,8 @@ int				main(int argc, char **argv)
 	}
 	else if (!ft_ps_read_stdin(&ps, NULL))
         ft_ps_read_in_txt(&ps, buf);
+//    ft_print_sw_tmp(ps.a->start);
+//    ft_print_sw_tmp(ps.b->start);
     ft_check_sorted(&ps);
 	return (0);
 }
